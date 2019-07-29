091208ab785791d359f5c4e80cfcbbdc4dcebcfa
muerwre <gotham48@gmail.com>
2019-07-29 17:44:43 +0700

    added sagaKiller

---
+++ sagas/saga-killer/sagas.ts
@@ -0,0 +1,16 @@
+// Runs saga on RUN_SAGA, but kills it on CANCEL_ALL
+
+import { call, race, take, takeLatest, fork } from 'redux-saga/effects';
+
+export function* sagaKiller(saga, actions, killer) {
+  yield takeLatest(actions, function* (...args) {
+    yield race({
+      task: call(saga, ...args),
+      cancel: take(killer),
+    });
+  });
+}
+
+function* mainSaga() {
+  yield fork(sagaKiller, workerSaga, ACTIONS.RUN_SAGA, ACTIONS.CANCEL_ALL);
+}
