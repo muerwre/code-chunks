b38d0b2c35fea36c0fbfc3b93fa1d7b8a6fc96af
Fedor Katurov <gotham48@gmail.com>
2020-03-17 11:19:38 +0700

    tons of fixes

--- configs/i3/.config/i3/locker.sh
+++ configs/i3/.config/i3/locker.sh
@@ -5,5 +5,5 @@ revert() {
 
 trap revert HUP INT TERM
 xset +dpms dpms 5 5 5
-i3lock -n -f -r 100 -c 222222
+i3lock -n -f -r 100 -c 222222 -i /home/muerwre/Pictures/wallpapers/leaves_plant_dark_131528_3840x2400.jpg1k
 revert
